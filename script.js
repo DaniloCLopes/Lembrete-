02fb4860d091231e67eb9aee14dc95ca36b940f2
DaniloFocus <danilocesarlopesfocus@outlook.com.br>
2024-06-07 15:50:13 -0300

    function adicionada

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 const input = document.querySelector("#new-task");
 const button = document.querySelector("#add-task");
 const list = document.querySelector("#task-list");
+
+button.addEventListener('click', function() {
+    
+    const taskText = input.value.trim();
+
+
+    if (taskText !== ''){
+
+        const newTask = document. 
+    }
+
+})
