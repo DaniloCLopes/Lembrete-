3294dec06b88585a8f60bf3c475239d5f8fb8b3a
DaniloFocus <danilocesarlopesfocus@outlook.com.br>
2024-06-07 15:57:58 -0300

    item li adicionado

--- script.js
+++ script.js
@@ -3,13 +3,16 @@ const button = document.querySelector("#add-task");
 const list = document.querySelector("#task-list");
 
 button.addEventListener('click', function() {
-    
+    //recupera valor do input
     const taskText = input.value.trim();
 
 
     if (taskText !== ''){
+        //cria um item <li>
+        const newTask = document.createElement('li');
+        newTask.textContent = taskText; 
 
-        const newTask = document. 
+        
     }
 
 })
